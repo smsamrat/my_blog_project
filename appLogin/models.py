4ea68533287474a817c4e5b1751f56dbef455d3a
you <97931972+smsamrat@users.noreply.github.com>
2022-03-17 13:25:52 +0600

    Create user profile model

--- appLogin/models.py
+++ appLogin/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+# Create your models here.
+
+class UserProfiles(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE, related_name='user_profiles')
+    profile_pics = models.ImageField(upload_to='profile_pics')
 
 
